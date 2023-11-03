d9035fa9a2d0b91c1e1480182842a03e88cc21dc
rdgronii <rgroner2@gmail.com>
2023-11-03 11:30:17 -0500

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Ricky David Groner II on 11/3/23.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
