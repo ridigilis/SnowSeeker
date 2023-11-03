d07bbda47a41ed2ca1cfd916473a0abdab33a5f9
rdgronii <rgroner2@gmail.com>
2023-11-03 12:08:16 -0500

    building a primary list of items

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    let resorts: [Resort] = Bundle.main.decode("resorts.json")
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List(resorts) { resort in
+                NavigationLink {
+                    Text(resort.name)
+                } label: {
+                    Image(resort.country)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 40, height: 50)
+                        .clipShape(
+                            RoundedRectangle(cornerRadius: 5)
+                        )
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(.black, lineWidth: 1)
+                        )
+                    
+                    VStack(alignment: .leading) {
+                        Text(resort.name)
+                            .font(.headline)
+                        Text("\(resort.runs) runs")
+                            .foregroundColor(.secondary)
+                    }
+                }
+            }.navigationTitle("Resorts")
         }
-        .padding()
     }
 }
 
